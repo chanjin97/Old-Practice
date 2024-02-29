54cf495899daf357894789cc3aa18a82dc47e6df
chanjin97 <chanjindark@naver.com>
2024-02-29 19:54:10 +0900

    Old Practice

---
+++ 02java/Plant.java
@@ -0,0 +1,12 @@
+class Plant{
+    void revolve(){
+        System.out.println("sun revolve");
+    }
+
+    public static void main(String[] args){
+            Plant earth = new Plant();
+            earth.revolve();
+    }
+
+}
+
